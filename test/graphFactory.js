f8386b7ac8f41371742a1db9ce3f1a77e4fbff17
jay <jbrundage372@gmail.com>
2016-02-02 15:04:02 -0500

    wip

--- test/graphFactory.js
+++ test/graphFactory.js
@@ -84,6 +84,59 @@
                     graph.data({ vertices: vertices, edges: edges });
                     callback(graph);
                 });
+            },
+            many_one_one_many: function (callback) {
+                require(["test/DataFactory", "src/graph/Graph", "src/common/Palette", "src/graph/Vertex", "src/graph/Edge"], function (DataFactory, Graph, Palette, Vertex, Edge) {
+                    var graph = new Graph();
+
+                    var vertices = [];
+                    var edges = [];
+
+                    var palette = Palette.ordinal("dark2");
+                    var nodes = DataFactory.Graph.simple.nodes;
+
+                    nodes.forEach(function (node,idx) {
+                            var annotation = [];
+                            if (Math.random() < 0.10) {
+                                annotation.push({"faChar": "A","tooltip": "Test A","shape_colorFill": "white","image_colorFill": "red"});
+                            }
+                            if (Math.random() < 0.10) {
+                                annotation.push({"faChar": "B","tooltip": "Test B","shape_colorFill": "green","image_colorFill": "white"});
+                            }
+                            if (Math.random() < 0.10) {
+                                annotation.push({"faChar": "C","tooltip": "Test C","shape_colorFill": "navy","image_colorFill": "white"});
+                            }
+                            vertices.push(new Vertex()
+                                .text(node.name)
+                                .textbox_shape_colorStroke(palette(node.group))
+                                .textbox_shape_colorFill("whitesmoke")
+                                .icon_shape_colorStroke(palette(node.group))
+                                .icon_shape_colorFill(palette(node.group))
+                                .annotationIcons(annotation)
+                                .faChar(node.name[0])
+                            );
+                            if(idx === 0){
+                                //edges.push(createEdge(vertices[0], vertices[link.target]).weight(link.value));
+                            } else if (idx === 1) {
+                                edges.push(createEdge(vertices[0], vertices[1]).weight(1));
+                            } else {
+                                edges.push(createEdge(vertices[idx], vertices[Math.floor(Math.random()+0.5)]).weight(1));
+                            }
+                    }, graph);
+
+                    function createEdge(source, target, label) {
+                        return new Edge()
+                            .sourceVertex(source)
+                            .targetVertex(target)
+                            .sourceMarker("circleFoot")
+                            .targetMarker("arrowHead")
+                            .text(label || "")
+                        ;
+                    }
+
+                    graph.data({ vertices: vertices, edges: edges }).layout("ForceDirected");
+                    callback(graph);
+                });
             }
         }
     };
