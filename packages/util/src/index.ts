668c362356cd12d7881a9a5b869c0fe6a7f08076
Gordon Smith <gordonjsmith@gmail.com>
2017-10-09 08:36:51 -0400

    Enable dynamic import
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -4,6 +4,7 @@ export * from "./dictionary";
 export * from "./esp";
 export * from "./graph";
 export * from "./hashSum";
+export * from "./loader";
 export * from "./logging";
 export * from "./object";
 export * from "./observer";
