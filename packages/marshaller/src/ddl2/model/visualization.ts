bafd7a03d4d1d6a8ab10b71547b46b4667815401
Jaman Brundage <jbrundage372@gmail.com>
2020-01-17 13:58:46 -0500

    feat(dashy): Add accessors to csv/tsv parser
    Signed-off-by: Jaman Brundage <jbrundage372@gmail.com>

--- packages/marshaller/src/ddl2/model/visualization.ts
+++ packages/marshaller/src/ddl2/model/visualization.ts
@@ -1,4 +1,4 @@
-import { Area, Bar, Bubble, Column, Contour, HexBin, Line, Pie, Radar, RadialBar, Scatter, Step, WordCloud } from "@hpcc-js/chart";
+import { Area, Bar, Bubble, Column, Contour, HexBin, Line, Pie, Radar, RadialBar, Scatter, StatChart, Step, WordCloud } from "@hpcc-js/chart";
 import { Database, EntityRectList, InputField, PropertyExt, publish, publishProxy, Widget } from "@hpcc-js/common";
 import { DDL2 } from "@hpcc-js/ddl-shim";
 import { Table } from "@hpcc-js/dgrid";
@@ -13,12 +13,12 @@ import { VizChartPanel } from "./vizChartPanel";
 export type VizType = "Table" | "FieldForm" |
     "Area" | "Bubble" | "Bar" | "Column" | "Contour" | "HexBin" | "Line" | "Pie" | "WordCloud" | "Radar" | "RadialBar" | "Scatter" | "Step" |
     "USCountiesChoropleth" | "USStatesChoropleth" | "ClusterPins" |
-    "EntityRectList" | "AdjacencyGraph";
+    "EntityRectList" | "AdjacencyGraph" | "StatChart";
 const VizTypeMap: { [key: string]: new (...args: any[]) => {} } = {
     Table, FieldForm,
     Area, Bubble, Bar, Column, Contour, HexBin, Line, Pie, Radar, RadialBar, Scatter, Step, WordCloud,
     USCountiesChoropleth: ChoroplethCounties, USStatesChoropleth: ChoroplethStates, ClusterPins: Leaflet.ClusterPins,
-    EntityRectList, AdjacencyGraph
+    EntityRectList, AdjacencyGraph, StatChart
 };
 export const VizTypeSet = [];
 for (const key in VizTypeMap) {
